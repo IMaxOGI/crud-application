d10d8f99f9d687f7b61234d95856155c543a3227
IMaxOGI <max165773@gmail.com>
2021-02-12 11:44:42 -0600

    update

---
+++ src/server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// API routers
+const userRouter = require("./api/users");
+
+const API_PORT = 3001;
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+app.use(jsonParser);
+
+app.get("/", (req, res) => {
+  res.send("Hello User!");
+});
+
+app.get("/echo", (req, res) => {
+  const text = req.query.q;
+  res.send(text);
+});
+
+let count = 0;
+
+app.get("/count", (req, res) => {
+  res.send(`Page views: ${++count}`);
+});
+
+app.use("/users", userRouter);
+
+app.listen(API_PORT, () => {
+  console.log(`Server is running on ${API_PORT} port`);
+});
